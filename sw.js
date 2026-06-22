d7e1915ee5ab17a6175746562c1c9cd6d37c3b74
n588nrsg95-dev <n588nrsg95@privaterelay.appleid.com>
2026-06-22 19:16:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fahrtenbuch-cache-v5-5';
-const ASSETS=['./','index.html','styles.css?v=5.5','app.js?v=5.5','manifest.webmanifest'];
+const CACHE='fahrtenbuch-v5-6';
+const ASSETS=['./','./index.html','./styles.css?v=5.6','./app.js?v=5.6','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request)))})
